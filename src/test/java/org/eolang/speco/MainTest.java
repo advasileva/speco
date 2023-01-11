5906a45d732aa047f4f34571133d453749044d15
advasileva <Alenadvasileva@yandex.ru>
2023-01-11 17:37:23 +0300

    fix: try fix

--- src/test/java/org/eolang/speco/MainTest.java
+++ src/test/java/org/eolang/speco/MainTest.java
@@ -110,7 +110,7 @@ public final class MainTest {
         if (SystemUtils.IS_OS_WINDOWS) {
             command = "cmd /c eoc link -s %s && eoc --alone dataize app && eoc clean";
         } else {
-            command = "eoc link -s %s";
+            command = "eoc link -s %s && eoc --alone dataize app";
         }
         System.out.println(command);
         final Process process = Runtime.getRuntime().exec(String.format(command, target));
